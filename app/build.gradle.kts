df84dd34ef5bd3a7536204080bc1d911e6f93b01
Adithya Gajulapally <adithya.gajulapally@gmail.com>
2024-11-16 10:04:06 -0800

    Add Compose support and cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,24 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
     namespace = "com.xai.helloworld"
     compileSdk = 34
 
+    buildFeatures {
+        compose = true
+    }
+
     defaultConfig {
         applicationId = "com.xai.helloworld"
         minSdk = 34
+        //noinspection OldTargetApi
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -36,11 +41,14 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    //compose bom
+    val composeBom = platform(libs.androidx.compose.bom)
+    implementation(composeBom)
+    // material 3
+    implementation(libs.androidx.material3)
+    // Android Studio Preview Support
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
+    // Compose Activity
+    implementation(libs.androidx.activity.compose)
 }
